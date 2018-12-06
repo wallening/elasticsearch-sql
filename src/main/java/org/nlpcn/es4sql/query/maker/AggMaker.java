25eb1750bc1b97d0b7ef86ca55e067681248a226
wallening <979673711@qq.com>
2018-12-06 11:18:45 +0800

    group by设置默认值为空

--- src/main/java/org/nlpcn/es4sql/query/maker/AggMaker.java
+++ src/main/java/org/nlpcn/es4sql/query/maker/AggMaker.java
@@ -68,7 +68,7 @@ public class AggMaker {
             }
             return makeRangeGroup(methodField);
         } else {
-            TermsAggregationBuilder termsBuilder = AggregationBuilders.terms(field.getName()).field(field.getName()).missing("N/A");
+            TermsAggregationBuilder termsBuilder = AggregationBuilders.terms(field.getName()).field(field.getName()).missing("");
             groupMap.put(field.getName(), new KVValue("KEY", termsBuilder));
             return termsBuilder;
         }
